43ad9622d9ba4e47e251080c98d53f0d5b834432
Yoshifumi <2g.4423@gmail.com>
2019-06-15 16:43:31 +0900

    update

--- js/Script.js
+++ js/Script.js
@@ -3,19 +3,15 @@ https://qiita.com/kouh/items/dfc14d25ccb4e50afe89
 */
 
 // 各DOM要素を取得する
-let nowSlide = document.getElementById('nowSlide');
-let prevSlide = document.getElementById('prevSlide');
-let nextSlide = document.getElementById('nextSlide');
+let ShowArea = document.getElementById('ShowArea');
   
 let slides = document.getElementsByClassName('slides');
 let slideSet = document.getElementsByClassName('slideSet');
 
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
+let nextButton = document.getElementById('nextButton');
+let prevButton = document.getElementById('prevButton');
 
-// スライドの表示順序を取得する
-function GetIndexs(){
-
+function GetNewSlide(nextOrPrev){
   let nowSlideIndex  = 0;
   let prevSlideIndex = 0;
   let nextSlideIndex = 0;
@@ -39,98 +35,104 @@ function GetIndexs(){
     nextSlideIndex = 0;
   }
 
-  let indexArray = {};
-  indexArray.now = nowSlideIndex;
-  indexArray.prev = prevSlideIndex;
-  indexArray.next = nextSlideIndex;
-  
-  return indexArray;
+  let nowSlide = slides[nowSlideIndex].firstElementChild.cloneNode(true);
+  let prevSlide = slides[prevSlideIndex].firstElementChild.cloneNode(true);
+  let nextSlide = slides[nextSlideIndex].firstElementChild.cloneNode(true);
+
+  let newSlide;
+  slides[nowSlideIndex].className = "slides";
+  if(nextOrPrev == "next"){
+    newSlide = nextSlide;
+    newSlide.style.width = "300px";
+    newSlide.style.height = "100%";
+    newSlide.style.position = "absolute";
+    newSlide.style.left = "300px";
+    slides[nextSlideIndex].className = "slides active";
+  }
+  if(nextOrPrev == "prev"){
+    newSlide = prevSlide;
+    newSlide.style.width = "300px";
+    newSlide.style.height = "100%";
+    newSlide.style.position = "absolute";
+    newSlide.style.left = "-300px";
+    slides[prevSlideIndex].className = "slides active";
+  }
+
+  return newSlide;
 }
 
-// スライドの画像をセットする
-function SetSlides(){
-
-  // スライドのインデックスを取得
-  index = GetIndexs();
-
-  // スライドの準備をする
-  let now  = slides[index.now].firstElementChild.cloneNode(true);
-  let prev = slides[index.prev].firstElementChild.cloneNode(true);
-  let next = slides[index.next].firstElementChild.cloneNode(true);
-
-  nowSlide.removeChild(nowSlide.firstElementChild);
-  prevSlide.removeChild(prevSlide.firstElementChild);
-  nextSlide.removeChild(nextSlide.firstElementChild);
-
+function SetNewSlide(slideElement){
   // スライドを表示エリアにセットする
-  nowSlide.appendChild(now);
-  prevSlide.appendChild(prev);
-  nextSlide.appendChild(next);
+  ShowArea.appendChild(slideElement);
 }
 
-// スライドを再度リセット
-function ResetSlide(){
-  // スライドのインデックスを取得
-  index = GetIndexs();
+// 画像を移動させる
+let x = 0;
+function MoveSlide(dist, newSlide){
+  // 指定位置まで移動したら止める
+  if(Math.abs(x) >= 300){
+    x = 0;
+    clearInterval(test.shift());
+    return;
+  }
 
-  // 表示するスライドのクラスを変更する
-  slides[index.now].className = "slides";
-  slides[index.next].className = "slides active";
-
-  SetSlides();
+  // 要素を移動する
+  let testx = newSlide.getBoundingClientRect().left;
+  x = x + dist;
+  newSlide.style.left = (testx + dist) + "px";
 }
 
 // setInterval用の変数定義
 let test = new Array();
 let AutoSlide = null;
 
-// 次の画像をスライドする
-function MoveNext(){
-  test.push(setInterval(function(){
-    MoveSlide(-1);
-  }, 1));
-}
-
-// 自動でスライドする
-function AutoMove(){
-  test.push(setInterval(function(){
-    MoveSlide(-1);
-  }, 1));
-}
-
-// 画像を移動させる
-let x = 0;
-function MoveSlide(dist){
-  // 指定位置まで移動したら止める
-  if(Math.abs(x) >= 300){
-    x = 0;
-    clearInterval(test.shift());
-    ResetSlide();
+function MoveNewSlide(nextOrPrev){
+  if(test.length > 0){
     return;
   }
-  // 要素を移動する
-  x = x + dist;
-  let newx =  x + "px";
-  nextSlide.firstElementChild.style.left = newx;
+
+  newSlide = GetNewSlide(nextOrPrev);
+  SetNewSlide(newSlide)
+
+  let moveDist = 0;
+
+  if(nextOrPrev == "next"){
+    moveDist = -1;
+  }
+
+  if(nextOrPrev == "prev"){
+    moveDist = 1;
+  }
+
+  test.push(setInterval(function(){
+    MoveSlide(moveDist, newSlide);
+  }, 1));
 }
 
 // 自動スライドのプリセット
-function PresetAutoMove(){
-  SetSlides();
+function SetAutoMove(){
   AutoSlide = setInterval(function(){
-    AutoMove();
+    MoveNewSlide("next");
   }, 5000);
 }
 
 // nextボタンクリック
-next.onclick = function(){
-  clearInterval(test.shift());
-  MoveNext();
+nextButton.onclick = function(){
+  clearInterval(AutoSlide);
+  MoveNewSlide("next");
+  SetAutoMove();
+}
+
+// prevボタンクリック
+prevButton.onclick = function(){
+  clearInterval(AutoSlide);
+  MoveNewSlide("prev");
+  SetAutoMove();
 }
 
 // 画面ロード時に自動スライドをセット
 window.onload = function(){
-  PresetAutoMove();
+  SetAutoMove();
 }
 
 // 画面から離れた時に自動スライドを切っておく
@@ -140,6 +142,5 @@ window.onblur = function(){
 
 // 画面に戻った時に自動スライドを再度セット
 window.onfocus = function(){
-  PresetAutoMove();
+  SetAutoMove();
 }
-
