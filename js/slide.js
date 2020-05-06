b7bc81c493e1fe291d7d44d34f136562ea2a8d8e
Yoshifumi-T <2g.4423@gmail.com>
2020-05-07 02:42:26 +0900

    自動スライド機能追加

--- js/slide.js
+++ js/slide.js
@@ -2,56 +2,31 @@
 
 // スライドのアニメーションを管理するクラス
 class SlideAnimator {
-    constructor(targetID) {
-        this.target    = document.getElementById(targetID);
+    constructor() {
+        this.target    = null;
         this.startTime = 0;
         this.startX    = 0;
         this.endX      = 0;
-        this.duration  = 0;
+        this.duration  = 1000;
     }
 
     start() {
         this.startTime = performance.now();
     }
 
-    setProparties(startX, endX, duration){
-        this.startX    = startX;
-        this.endX      = endX;
-        this.duration  = duration;
+    setTarget(e) {
+        this.target = e;
     }
 
-    prev() {
-        const reqID = requestAnimationFrame(this.prev.bind(this));
-
-        const elapsed = performance.now() - this.startTime;
-        const left = this.getProgress('linear', elapsed, this.startX, this.endX, this.duration);
-        this.target.style.left = `${left}px`;
-        document.getElementById('target1').innerHTML = `left: ${left}`;
-
-        if(left >= this.endX) {
-            cancelAnimationFrame(reqID);
-            this.target.style.left = `${this.endX}px`;
-        }
-    }
-
-    next() {
-        const reqID = requestAnimationFrame(this.next.bind(this));
-
-        const elapsed = performance.now() - this.startTime;
-        const left = this.getProgress('linear', elapsed, this.startX, this.endX, this.duration);
-        this.target.style.left = `${left}px`;
-
-        if(left <= this.endX) {
-            cancelAnimationFrame(reqID);
-            this.target.style.left = `${this.endX}px`;
-        }
+    setDuration(d) {
+        this.duration = d;
     }
 
     appear() {
         const reqID = requestAnimationFrame(this.appear.bind(this));
 
         const elapsed = performance.now() - this.startTime;
-        const opacity = this.getProgress('linear', elapsed, 0, 1, 1000);
+        const opacity = this.getProgress('linear', elapsed, 0, 1, this.duration);
         this.target.style.opacity = `${opacity}`;
 
         if(opacity >= 1) {
@@ -87,61 +62,84 @@ class SlideAnimator {
 
 // スライドを管理するクラス
 class Slide {
-    constructor(targetID, prevBtnID, nextBtnID, animator) {
+    constructor(targetID, prevBtnID, nextBtnID, animator, images) {
         this.target   = document.getElementById(targetID);
         this.prevBtn  = document.getElementById(prevBtnID);
         this.nextBtn  = document.getElementById(nextBtnID);
         this.animator = animator;
+        this.images   = images;
         this.index    = 0;
-        this.images   = ['../img/sample1.jpeg', '../img/sample2.jpeg', '../img/sample3.jpeg', '../img/sample4.jpeg', '../img/sample5.jpeg'];
+        this.timer    = null;
         this.prevBtn.addEventListener('click', this.onclickPrevBtn.bind(this));
         this.nextBtn.addEventListener('click', this.onclickNextBtn.bind(this));
         document.querySelectorAll('.navi-item').forEach(e => {
             e.addEventListener('click', this.onclickNavi.bind(this));
         });
 
-        this.run(this.index);
+        this.update(this.index);
     }
 
-    setSlideAnimator(animator) {
+    setAnimator(animator) {
         this.animator = animator;
     }
 
-    setSlide() {
+    setAutoAnimation() {
+        this.timer = setInterval(this.onclickNextBtn.bind(this), 5000);
+    }
+
+    resetAutoAnimation() {
+        clearInterval(this.timer);
+        this.setAutoAnimation();
+    }
+
+    onclickPrevBtn() {
+        if(this.animator === null) return;
+        this.update(this.index - 1);
+    }
+
+    onclickNextBtn() {
+        if(this.animator === null) return;
+        this.update(this.index + 1);
+    }
+
+    onclickNavi(e) {
+        const index = Number(e.target.dataset.index);
+        this.update(index);
+    }
+
+    update(index) {
+        this.resetAutoAnimation()
+        this.updateIndex(index)
+        this.updateSlide();
+        this.updateNavi();
+    }
+
+    updateIndex(index) {
+        if(index < 0) {
+            this.index = this.images.length -1;
+            return;
+        }
+
+        if(index >= this.images.length) {
+            this.index = 0;
+            return;
+        }
+        
+        this.index = index;
+    }
+
+    updateSlide() {
         const e = document.createElement('img');
         e.setAttribute('src', this.images[this.index]);
         e.style.opacity  = 0;
         e.style.position = 'absolute';
         this.target.insertAdjacentElement('beforeend', e);
-        this.animator.target = e;
+        this.animator.setTarget(e);
         this.animator.start();
         this.animator.appear();
     }
 
-    onclickPrevBtn() {
-        if(this.animator === null) return;
-        if(this.index === 0) return;
-        this.run(this.index - 1);
-    }
-
-    onclickNextBtn() {
-        if(this.animator === null) return;
-        if(this.index === this.images.length -1) return;
-        this.run(this.index + 1);
-    }
-
-    onclickNavi(e) {
-        const index = Number(e.target.dataset.index);
-        this.run(index);
-    }
-
-    run(index) {
-        this.index = index;
-        this.setSlide();
-        this.setNavigation();
-    }
-
-    setNavigation() {
+    updateNavi() {
         const navi = document.querySelectorAll('.navi-item');
         for(let i = 0; i < navi.length; i++) {
             navi[i].style.background = '#CCCCCC';
@@ -150,28 +148,14 @@ class Slide {
             }
         }
     }
-
-    // prev() {
-    //     const start = this.left;
-    //     const end   = start + 500;
-    //     const time  = 1000;
-    //     this.slideAnimator.setProparties(-1000, -500, time);
-    //     this.slideAnimator.start();
-    //     this.slideAnimator.prev();
-    //     this.left = end;
-    // }
-
-    // next() {
-    //     const start = this.left;
-    //     const end   = start - 500;
-    //     const time  = 1000;
-    //     this.slideAnimator.setProparties(-500, -1000, time);
-    //     this.slideAnimator.start();
-    //     this.slideAnimator.next();
-    //     this.left = end;
-    // }
-
 }
 
-const animator = new SlideAnimator('js-slide');
-const slide = new Slide('js-slide', 'js-prev-btn', 'js-next-btn', animator);
+const animator = new SlideAnimator();
+const images = [
+    '../img/sample1.jpeg', 
+    '../img/sample2.jpeg', 
+    '../img/sample3.jpeg', 
+    '../img/sample4.jpeg', 
+    '../img/sample5.jpeg'
+];
+const slide = new Slide('js-slide', 'js-prev-btn', 'js-next-btn', animator, images);
